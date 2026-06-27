f2221f910fb74f14b974fcc50462c00920ed9f92
Anibal <ubilla.anibal@gmail.com>
2026-06-27 09:41:12 +0200

    enables uploading images on decap dashboard

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,20 +3,19 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const projects = defineCollection({
-	// Load Markdown and MDX files in the `src/content/projects/` directory.
-	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			heroImage: z.optional(image()),
-			tags: z.array(z.string()).optional(),
-			liveUrl: z.string().optional(),
-			repoUrl: z.string().optional(),
-		}),
+    // Load Markdown and MDX files in the `src/content/projects/` directory.
+    loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
+    // Type-check frontmatter using a schema
+    schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            // Transform string to Date object
+            pubDate: z.coerce.date(),
+            heroImage: z.string().optional(),
+            tags: z.array(z.string()).optional(),
+            liveUrl: z.string().optional(),
+            repoUrl: z.string().optional(),
+        }),
 });
 
 export const collections = { projects };
